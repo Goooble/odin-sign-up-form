21dcf3fc3ca8b390a36f61f423db417fa5fcba14
Goooble <[bivinbheemaiah@gmail.com]>
2024-08-12 19:11:32 +0530

    Display two errors for invalid email address
    one error for not being filled index
    another for invalid email address

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const create = document.querySelector(".create-container button");
-const required = document.querySelectorAll("input")
+const required = document.querySelectorAll("input");
+const email = document.querySelector("#email");
 
-create.addEventListener("click", ()=> {
-    required.forEach((ele) => ele.className = "required");
+create.addEventListener("click", () => {
+  //only just so that this gets triggered after the create account
+  //button has been pressed
+
+  required.forEach((ele) => ele.classList.add("required"));
 });
+email.classList.toggle("email-entered"); //to turn it off by default
+email.addEventListener("focusout", () => {
+    if(email.value !== ""){
+        if(!email.classList.contains("email-entered")){
+        email.classList.toggle("email-entered");
+        }
+    }
+    else{
+        if(email.classList.contains("email-entered")){
+        email.classList.toggle("email-entered");
+        }
+    }
+});
+
